f70f92ba6d85ae2e2d60d9d32260329551f86675
shyjal <shyjal99@gmail.com>
2015-02-28 18:58:36 +0530

    --first

---
+++ app/app.js
@@ -0,0 +1,34 @@
+(function() {
+
+    var memebers = [{
+        name: 'Aslam',
+        avatar: 'assets/img/avatar1.png'
+    }, {
+        name: 'Seb',
+        avatar: 'assets/img/avatar2.png'
+    }, {
+        name: 'Ibru',
+        avatar: 'assets/img/avatar3.png'
+    }];
+
+    var app = angular.module('myapp', []);
+
+
+    app.controller('teamMember', ['$scope', function($scope) {
+            $scope.team = memebers;
+
+
+
+        }]).directive('header', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/views/header.html'
+            };
+        })
+        .directive("footer", function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/views/footer.html'
+            };
+        });
+})();
